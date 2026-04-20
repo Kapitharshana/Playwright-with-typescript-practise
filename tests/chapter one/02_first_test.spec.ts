955d13a39434f402d809cd5beed6f60917efec29
Kapitharshana <kapidarsha431@gmail.com>
2026-04-21 00:19:50 +0530

    second day

--- tests/chapter one/02_first_test.spec.ts
+++ tests/chapter one/02_first_test.spec.ts
@@ -1,5 +1,4 @@
-import {test, expect, Locator } from "@playwright/test";
-   
+import { test, expect } from '@playwright/test';
 
 /* Go to google.com
 type playwright by testrs talk and search it
@@ -14,7 +13,9 @@ await page.goto('https://www.google.com');
 await page.getByRole('combobox', { name: 'Search' }).fill('playwright by testers talk');
 await page.getByRole('combobox', { name: 'Search' }).press('Enter');
 
+//await page.getByRole('link', { name: 'Playwright by Testers Talk YouTube · Testers Talk 29.2K+ followers' }).first().click();
+await page.getByRole('link', { name: /Playwright by Testers Talk/i }).first().click();
 
-
+await expect(page).toHaveTitle("Playwright by Testers Talk - YouTube");
 
 })
