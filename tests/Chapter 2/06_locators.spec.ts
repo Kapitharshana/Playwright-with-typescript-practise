b319d63f034b6ce475c9e9c1f685c18ea8785109
Kapitharshana <kapidarsha431@gmail.com>
2026-04-23 00:51:10 +0530

    css selector, xpath and placeholder

--- tests/Chapter 2/06_locators.spec.ts
+++ tests/Chapter 2/06_locators.spec.ts
@@ -1,7 +1,7 @@
 import{test,expect} from '@playwright/test';
 
 test('locators', async ({ page }) => {
-  await page.goto('https://github.com/Kapitharshana');
+ /* await page.goto('https://github.com/Kapitharshana');
 
   //await page.getByRole('link', { name: 'Sign in' }).click();
 
@@ -11,7 +11,21 @@ test('locators', async ({ page }) => {
 
   //await page.getByTestId('repositories').first().click();
 
-  await page.getByLabel('Homepage').first().click();;
+  //await page.getByLabel('Homepage').first().click();;
  
+  //Use placeholder 
+  await page.getByPlaceholder('Search or jump to...').click();
+  ///here can't use fill using plaaceholder option bcz this search bar is a button
 
+  //use x path  ]
+  await page.locator("//button[@placeholder='Search or jump to...']").click();
+
+  //use css selector 
+  await page.locator("button[placeholder='Search or jump to...']").click();
+*/
+//
+ await page.goto('https://www.youtube.com');
+ //await page.getByPlaceholder('Search').fill("testers talk");
+ //await page.locator("//input[@placeholder='Search']").fill("testers talk"); //xpath
+ //await page.locator("input[placeholder='Search']").fill("testers talk");    //css selector
 })
