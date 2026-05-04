baa1521af5d36737b53600b143544d1fada93b1f
Kapitharshana <kapidarsha431@gmail.com>
2026-05-04 20:56:13 +0530

    soft assertion

---
+++ tests/Chapter 2/12_Hard_assertions.spec.ts
@@ -0,0 +1,30 @@
+import{test,expect} from '@playwright/test';
+import { title } from 'process';
+
+test('Hardassertions', async ({ page }) => {
+
+  await page.goto('https://www.youtube.com/');
+
+  //visible , enabled, empty, editable
+  /*
+  await expect (page.getByPlaceholder('Search')).toBeVisible();
+ await expect (page.getByPlaceholder('Search')).toBeEnabled();
+ await expect (page.getByPlaceholder('Search')).toBeEmpty();
+ await expect (page.getByPlaceholder('Search')).toBeEditable();
+
+*/
+ //URL check 
+ await page.getByPlaceholder('Search', {exact:true}).first().click();
+ await page.getByPlaceholder('Search', {exact:true}).first().fill('playwright by testers talk');
+ await page.getByPlaceholder('Search', {exact:true}).first().press('Enter');
+//await expect(page).toHaveURL('https://www.youtube.com/results?search_query=playwright+by+testers+talk');
+
+//text check 
+//await expect(page.locator('//span[@id="title"]')).toHaveText('People also watched');
+
+//await expect(page.locator('//span[@id="title"]')).toHaveCount(1);
+//await expect(page.locator('(//span[@id="title"])[1]')).toBeDisabled(); -> currently can't use this disabled mathoed for span , text but can use for button ,input, slect , textarea
+
+await expect(page).toHaveTitle('playwright by testers talk - YouTube');
+
+})   
--- tests/Chapter 2/14_Soft_assertion.spec.ts
+++ tests/Chapter 2/14_Soft_assertion.spec.ts
@@ -1,30 +1,24 @@
 import{test,expect} from '@playwright/test';
 import { title } from 'process';
 
-test('assertions', async ({ page }) => {
+test('Soft assertions', async ({ page }) => {
 
   await page.goto('https://www.youtube.com/');
 
-  //visible , enabled, empty, editable
-  /*
-  await expect (page.getByPlaceholder('Search')).toBeVisible();
- await expect (page.getByPlaceholder('Search')).toBeEnabled();
- await expect (page.getByPlaceholder('Search')).toBeEmpty();
- await expect (page.getByPlaceholder('Search')).toBeEditable();
 
-*/
  //URL check 
  await page.getByPlaceholder('Search', {exact:true}).first().click();
  await page.getByPlaceholder('Search', {exact:true}).first().fill('playwright by testers talk');
  await page.getByPlaceholder('Search', {exact:true}).first().press('Enter');
 //await expect(page).toHaveURL('https://www.youtube.com/results?search_query=playwright+by+testers+talk');
 
+await expect.soft(page).toHaveTitle('playwright talk - YouTube'); //this one os wrong but we mention it as soft , so the rest of the codes will be executed wihtout any problem and at the end of the test it will show us that this assertion is failed but the rest of the assertions are passed
 //text check 
 await expect(page.locator('//span[@id="title"]')).toHaveText('People also watched');
 
 await expect(page.locator('//span[@id="title"]')).toHaveCount(1);
-//await expect(page.locator('(//span[@id="title"])[1]')).toBeDisabled(); -> currently can't use this disabled mathoed for span , text but can use for button ,input, slect , textarea
+await expect(page.locator('(//span[@id="title"])[1]')).toBeDisabled(); //-> currently can't use this disabled mathoed for span , text but can use for button ,input, slect , textarea
 
-await expect(page).toHaveTitle('playwright by testers talk - YouTube');
+
 
 })   
