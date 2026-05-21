d979fc26750145a4f02954a32a9de9ee404ad97e
Kapitharshana <kapidarsha431@gmail.com>
2026-05-21 21:39:10 +0530

    message

---
+++ tests/chapter4/26_rerun_failedtestcase.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+
+test('test1', async ({ page }) => {
+   test.setTimeout(120000);
+  await page.goto('https://www.youtube.com/');
+  await page.getByRole('combobox', { name: 'Search' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).fill('playwright by testers talk');
+  await page.getByRole('combobox', { name: 'Search' }).press('Enter');
+  await expect(page.locator("span:has-text('Playwright by Testers Talk')")).toBeVisible();
+
+});
+ 
+test('test2', async ({ page }) => {
+   expect(true).toBe(true);
+
+});
+
+test('test3', async ({ page }) => {
+   expect(true).toBe(true);
+
+});
+
+/* intentionally failing the testcase to check the rerun failed testcases  
+ //for that running the code in terminal which is : npx playwright test --last-failed
+test('test2', async ({ page }) => {
+   expect(true).toBe(false);
+
+});
+
+test('test3', async ({ page }) => {
+   expect(true).toBe(false);
+
+});
+*/
